b689a115a293798e622929e79ed632c7d9de67db
ezgi-karaca <w.ezgikaraca@gmail.com>
2025-04-12 05:21:25 +0300

    live backend added

--- taskmanager-frontend/src/api/api.js
+++ taskmanager-frontend/src/api/api.js
@@ -1,7 +1,9 @@
-import axios from 'axios';
+import axios from "axios";
 
 const API = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL + "/api",
 });
+
+export const login = (data) => API.post("/auth/login", data);
 
 export default API;
